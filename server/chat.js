906f73efcd4bb4827f97d75ab083e9e1bdd5ac12
kyLee <kyungim.lee@koscom.co.kr>
2021-10-10 15:31:10 +0900

    자바스크립트 Tensorflow 라이브러리 추가

--- server/chat.js
+++ server/chat.js
@@ -12,31 +12,27 @@ const router = express.Router();
 router.post("/", (req, res, next) => {
   try {
     // 요청객체 유효성 체크
-    if (!req || !res || !req.body || !req.body.message) {
-      next(createError(405, strings.err_wrong_params("message")));
+    if (!req || !res || !req.body) {
+      next(createError(405, strings.err_wrong_params));
       return;
     }
-
-    // 요청객체 값 추출
-    const { message } = req.body;
-    // chat.checkParamValid(req.body);
+    const chat = new ChatHandler(message, 
+        (answer) => {
+          const resBody = {
+            message: message, // 기존 메시지
+            answer: answer // 챗봇 답변
+          };
+    
+          res.status(200).json(resBody);
+          logger.debug("RESPONSE", resBody, req.originalUrl);
+        }, 
+        (error) => {
+          next(createError(520, strings.err_chat_fail));
+        });
+    chat.checkParamValid(req.body);
     logger.debug("REQUEST", req.body, req.originalUrl);
 
-    const successCallback = (answer) => {
-
-      const resBody = {
-        message: message, // 기존 메시지
-        answer: answer // 챗봇 답변
-      };
-
-      res.status(200).json(resBody);
-      logger.debug("RESPONSE", resBody, req.originalUrl);
-    };
-    const errorCallback = (error) => {
-      next(createError(520, strings.err_chat_fail));
-    };
-    const chat = new ChatHandler(message, successCallback, errorCallback);
-
+    
 
   } catch (e) {
     next(e);
